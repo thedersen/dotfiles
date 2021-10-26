1541af1b6b82ab78b80a04f4d6435d4846d1a9c9
Thomas Pedersen <thedersen@gmail.com>
2021-10-26 13:32:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,24 +2,19 @@
 xcode-select --install
 
 # Install Homebrew
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install git
 brew install git
 
 # Make some directories
-mkdir -p ~/dev/dotfiles
-mkdir -p ~/dev/os
-mkdir -p ~/dev/play
-mkdir -p ~/dev/work
-mkdir -p ~/dev/talk
-mkdir -p ~/dev/me
+mkdir -p ~/dev
 
 # Clone this repository
 git clone https://github.com/thedersen/dotfiles.git ~/dev/dotfiles
 
 # Install n
-curl -L https://git.io/n-install | zsh
+curl -L https://git.io/n-install | bash
 
 # Run other install scripts
 source ~/dev/dotfiles/brew.sh
